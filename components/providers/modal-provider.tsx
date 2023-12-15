b3b0fb1fd4cbaace1b3a544dbc49763510594bcf
bharadwaj1118 <bharadwaj.kanukoti@gmail.com>
2023-12-15 17:57:20 +0100

    Added create server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,8 +2,10 @@
 
 import { useEffect, useState } from 'react';
 
-import { CreateServerModal } from '@/components/modals/create-server-modal';
+import { EditServerModal } from '@/components/modals/edit-server-modal';
 import { InviteModal } from '@/components/modals/invite-modal';
+import { CreateServerModal } from '@/components/modals/create-server-modal';
+import { CreateChannelModal } from '../modals/create-channel-model';
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -20,6 +22,8 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
+      <CreateChannelModal />
     </>
   );
 };
